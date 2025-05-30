6b8b725de7570e33d64fb8798bab42ce00d26d7a
Abdo <83555913+abdoibrahim257@users.noreply.github.com>
2025-05-30 18:46:04 +0300

    returned everything to norm

--- my-app/components/UploadForm.jsx
+++ my-app/components/UploadForm.jsx
@@ -9,7 +9,6 @@ const UploadForm = () => {
   const [language, setLanguage] = useState('');
 
   // Use environment variable for API URL
-  const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
   function handelFileSelect(event) {
     const file = event.target.files[0];
@@ -46,7 +45,7 @@ const UploadForm = () => {
       return;
     }
 
-    fetch(`${API_URL}/getSRT/${transID}`, {
+    fetch(`https://full-stack-alchemist-production.up.railway.app/getSRT/${transID}`, {
       method: 'GET',
       headers: {
         'Accept': 'application/srt',
@@ -101,7 +100,7 @@ const UploadForm = () => {
 
     setLoading(true);
 
-    fetch(`${API_URL}/uploadfile`, {
+    fetch(`https://full-stack-alchemist-production.up.railway.app/uploadfile`, {
       headers: {
         'Accept': 'application/json',
       },
